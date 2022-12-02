cea460b5d8ef9d189540cc1a310763c9a9689f08
HonorioMTH <matheus.honorio88@gmail.com>
2022-12-02 09:11:16 -0300

    task 1

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
+import Home from './paginas/home/Home';
+import Sobre from './paginas/sobre nós/Sobre';
+
+function App() {
+  return (
+      <Router>
+        <Navbar />
+          <Routes>
+              {/* <Route path='/' element={<Login />}/> */}
+              <Route path='/home' element={<Home />}/>
+              <Route path='/sobre' element={<Sobre />}/>
+              {/* <Route path='/cadastrousuario' element={<CadastroUsuario />}/> */}
+          </Routes>
+        <Footer /> 
+      </Router>
+  );
+}
+
+export default App;
